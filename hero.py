764789a58f9af9bbb90f4ee2364fe544f87efe86
Quentin LOUISIADE <qlouisia@e2r2p7.42.fr>
2019-01-05 15:53:35 +0100

    add action_fuction and moving monster

--- hero.py
+++ hero.py
@@ -1,9 +1,9 @@
 from random import randint
-
+import actions_function
 class Hero:
     def __init__(self,x,y,lvl):
        self.hp = 12 + (lvl * 2)
-       self.str = 12 + lvl
+       self.str = (12 + lvl) / 2
        self.armor = 0
        self.x = x
        self.y = y
@@ -24,18 +24,20 @@ class Hero:
             self.combat_status = "you hit " + enemy.name + " [" + str(self.damage) + " damage(s)]"
         else:
             self.combat_status = "you miss " + enemy.name
+        if (enemy.hp <= 0):
+            self.combat_status += " | Defeate " + enemy.name
 enemy_list = [
-    { "name": "Bat", "hp": 10, "str":8,"armor":0,"symbol": "B","acc": 4,"def": 8 },
-    { "name": "Snake", "hp": 10, "str":11,"armor":0,"symbol": "S","acc": 6,"def": 7 },
-    { "name": "Gobelin", "hp": 12, "str":8,"armor":0,"symbol": "G","acc": 5,"def": 9 },
-    { "name": "Hobgobelin", "hp": 14, "str":14,"armor":1,"symbol": "H","acc": 5,"def": 11 },
+    { "name": "Bat", "hp": 16, "str":8,"armor":0,"symbol": "B","acc": 4,"def": 8 },
+    { "name": "Snake", "hp": 16, "str":11,"armor":1,"symbol": "S","acc": 6,"def": 7 },
+    { "name": "Gobelin", "hp": 16, "str":8,"armor":2,"symbol": "G","acc": 5,"def": 9 },
+    { "name": "Hobgobelin", "hp": 16, "str":14,"armor":2,"symbol": "H","acc": 5,"def": 11 },
 ]
 
 class Enemy:
     def __init__(self,x,y,lvl,index):
        self.name= enemy_list[index]["name"]
        self.hp = enemy_list[index]["hp"] + (lvl * 2)
-       self.str = enemy_list[index]["str"]+ lvl
+       self.str = (enemy_list[index]["str"]+ lvl) / 2
        self.armor = enemy_list[index]["armor"]
        self.x = x
        self.y = y
@@ -44,26 +46,44 @@ class Enemy:
        self.accuracy = enemy_list[index]["acc"] + lvl
        self.defense = enemy_list[index]["def"] + lvl/2
        self.combat_status = ""
+       self.pursuit = False
+       self.mouvement = 1
     
-    def attack(self,enemy):
-        if ((randint(0,20) + self.accuracy) > enemy.defense):
-            self.damage = self.str - enemy.armor
+    def attack(self,hero):
+        if ((randint(0,20) + self.accuracy) > hero.defense):
+            self.damage = self.str - hero.armor
             if (self.damage < 0):
                 self.damage = 1
-            enemy.hp -= self.damage
+            hero.hp -= self.damage
             self.combat_status = self.name + " hit you" + " [" + str(self.damage) + " damage(s)]"
         else:
             self.combat_status =  self.name + " miss you"
+    
+    def pursuit(self,hero):
+        if(actions_function.get_distance(self,hero) > 10):
+            self.pursuit = True
+
+    def move(self,hero):
+        if (self.x > hero.x):
+            self.x = self.x - self.mouvement;
+        elif (self.x < hero.x):
+            self.x = self.x + self.mouvement;
+        if (self.y > hero.y):
+            self.y = self.y - self.mouvement;
+        elif (self.y < hero.y):
+            self.y = self.y + self.mouvement;
+        
+        
+p1 = Hero(0,30,2)
+p2 = Enemy(0,0,1,3)
 
 
-p1 = Hero(0,0,2)
-p2 = Enemy(0,0,1,1)
-
+while (p1.x != p2.x or p1.y != p2.y):
+    p2.move(p1)
+    print("p2 x :"+ str(p2.x))
+    print("p2 y :"+ str(p2.y))
 
 """
-print(p2.name)
-print(p2.defense)
-
 while (p1.hp > 0 and p2.hp > 0):
     
     print(p1.hp)
