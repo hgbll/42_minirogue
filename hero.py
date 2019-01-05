1d0586c77a9e2d43887be312316694230ccdfb04
Quentin LOUISIADE <qlouisia@e2r2p7.42.fr>
2019-01-05 12:05:27 +0100

    added hero.py removed pyc

---
+++ hero.py
@@ -0,0 +1,21 @@
+from random import randint
+
+class Hero:
+    def __init__(self,x,y,lvl):
+       self.hp = 12 + (lvl * 2)
+       self.str = 12 + lvl
+       seff.armor = 0
+       self.x = x
+       self.y = y
+       self.xp = 0
+       self.lvl = lvl
+       self.accuracy = 5 + lvl
+       self.defense = 10 + lvl
+       self.combat_status = ""
+    
+    def attack(self,enemy)
+        if ((randint(0,20) + self.accuracy) > enemy.defense)
+            self.combat_status = "you hit"
+			enemy.hp -= self.str
+        else
+            self.combat_status = "you miss"
